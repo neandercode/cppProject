a061fb05a52a50a3634495cc26e91ea0a87dbdc8
neandercode <josephjones555@yahoo.com>
2024-01-08 17:17:40 -0500

    for loop

---
+++ loop.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string anime[5] = {"dbz", "attack on titan", "one piece", "bungo stray dogs", "black clover"};
+    for (int i = 0; i < 5; i++) {
+        cout << anime[i] << "\n";
+    }
+}
