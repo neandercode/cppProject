a7426a2fc36f2de0970fb9a2e3b8db1d5883c3df
neandercode <josephjones555@yahoo.com>
2024-01-09 05:25:39 -0500

    structures

---
+++ struct.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    struct
+    {
+        string brand;
+        string model;
+        int year;
+    } myCar1, myCar2;
+
+    myCar1.brand = "Ford";
+    myCar1.model = "Fuision";
+    myCar1.year = 2019;
+
+    myCar2.brand = "Ford";
+    myCar2.model = "Mustang GT";
+    myCar2.year = 2024;
+
+    cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
+    cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
+    return 0;
+}
