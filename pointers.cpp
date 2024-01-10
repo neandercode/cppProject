b4e19aa9c667e7b66f69eb7a729d2c8e12d5e016
neandercode <josephjones555@yahoo.com>
2024-01-10 03:40:11 -0500

    pointers

---
+++ pointers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string anime = "One Piece";
+    string* ptr = &anime;
+
+    cout << anime << "\n";
+
+    cout << &anime << "\n";
+
+    cout << ptr << "\n";
+    return 0;
+}
