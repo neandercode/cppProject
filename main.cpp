eb1b9f003cf79b01369feff353089b0f1cc83636
neandercode <josephjones555@yahoo.com>
2024-01-11 04:29:06 -0500

    calling a function to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
+
+void myFunction() {
+    cout << "...and thats my string";
+}
 int main()
 {
     string cars[4] = {"Ford", "Chevrolet", "Dodge", "Jeep"};
     cars[0] = "Lincoln";
     cout << cars[0];
+
+    myFunction();
     return 0;
 }
