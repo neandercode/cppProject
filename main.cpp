8a7e5fcd0a08cdc2d2c30489e989f64e20063714
neandercode <josephjones555@yahoo.com>
2024-01-11 04:44:01 -0500

    created a second function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void myFunction() {
+void myFunction()
+{
     cout << "...and thats my string";
 }
-int main()
+void myBefore()
 {
+    cout << "Here is what I recommend: \n";
+}
+int main()
+
+{
+    myBefore();
+
     string cars[4] = {"Ford", "Chevrolet", "Dodge", "Jeep"};
     cars[0] = "Lincoln";
     cout << cars[0];
