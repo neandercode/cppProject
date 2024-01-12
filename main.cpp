b70cc12630720921b04b21639ccc9960dcb09d95
neandercode <josephjones555@yahoo.com>
2024-01-12 05:17:36 -0500

    definition below main

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void myFunction()
-{
-    cout << "...and thats my string";
-}
-void myBefore()
-{
-    cout << "Here is what I recommend: \n";
-}
+void myFunction();
+
+
 int main()
 
 {
-    myBefore();
-
     string cars[4] = {"Ford", "Chevrolet", "Dodge", "Jeep"};
     cars[0] = "Lincoln";
     cout << cars[0];
 
     myFunction();
     return 0;
+}
+
+void myFunction()
+{
+    cout << "...and thats my string";
 }
